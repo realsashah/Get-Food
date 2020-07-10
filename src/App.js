d42f836f3ef84631bf14501f0da78c611555101f
ahsanshah <shahahsan56@gmail.com>
2020-07-10 21:15:47 +0500

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import './App.css';
+import Header from './components/HeaderComponent';
 import Footer from './components/Footercomponents';
 import About from './components/Aboutcomponents';
 function App() {
   return (
+    <>
+    <Header/>
     <About/>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,78 @@
+import React,{Component} from 'react';
+import { Navbar,NavbarBrand,NavItem,NavLink,NavbarToggler,Collapse,Nav,Jumbotron,
+Button}from 'reactstrap';
+import {Link} from 'react-router-dom';
+
+class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isNavOpen:false
+        };
+
+        this.toggleNav=this.toggleNav.bind(this);
+
+    }
+
+    toggleNav(){
+        this.setState({
+            isNavOpen:!this.state.isNavOpen
+        });
+    }
+
+    render(){
+        return(
+            <>
+                <Navbar dark expand='md'>
+                <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className='mr-auto' href='/'>
+                            Get Food
+                        </NavbarBrand>  
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className='nav-link' to="/home"><span className='fa fa-home fa-lg'></span>Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to="/aboutus"><span className='fa fa-info fa-lg'></span>About Us</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to="/menu"><span className='fa fa-list fa-lg'></span>Menu</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className='nav-link' to="/contactus"><span className='fa fa-address-card fa-lg'></span>Contact Us</NavLink>
+                            </NavItem>
+                        </Nav>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <Button outline onClick={this.toggleModal}>
+                                    <span className="fa fa-sign-in fa-lg"></span>
+                                    Login
+                                </Button>
+                            </NavItem>
+
+                        </Nav>
+                        </Collapse>
+                </Navbar>
+
+                <Jumbotron>
+                     <div className='container'>
+                        <div className='row row-header'>
+                            <div className='col-12 col-sm-6'>
+                                <h1>Get Food</h1>
+                                <h3>We are here for you.</h3>
+                            </div>
+
+                        </div>
+
+
+                     </div>
+                </Jumbotron>
+
+            </>
+        );
+    }
+}
+
+
+export default Header;
