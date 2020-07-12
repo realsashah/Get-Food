03af7824b5ba82a20fb44a9e67404d50be2230ac
ahsanshah <shahahsan56@gmail.com>
2020-07-12 13:23:53 +0500

    Added Pictures

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,15 @@
 import React,{Component} from 'react';
 import {Media} from 'reactstrap';
 import {baseUrl} from '../shared/baseUrl';
-import ahsanshah from '../assets/img/ahsanshah.jpg';
-import food from '../assets/img/pakistaniFood.jpg';
-import pizza from '../assets/img/pizza.jpg';
+import {DISHES} from '../shared/dishes';
+import {LEADERS} from '../shared/leaders';
+import {PROMOTIONS} from '../shared/promotions';
 
 
+const dish=DISHES.filter(d=>d.featured)[0];
+
+const leader=LEADERS.filter(lead=>lead.featured)[0];
+const promo=PROMOTIONS.filter(p=>p.featured)[0];
 
 function Home(props){
     return(
@@ -18,13 +22,13 @@ function Home(props){
             </div>
             <div className="col col-sm order-sm-first col-md">
                 <div className="media">
-                    <img className="d-flex mr-3 img-thumbnail align-self-center" src={pizza} alt="uthappizza" />
+                    <img className="d-flex mr-3 img-thumbnail align-self-center" src={dish.image} alt="uthappizza" />
                     <div className="media-body">
-                        <h2 className='mt-0'>Extra Large Pizza
+                        <h2 className='mt-0'>{dish.name}
                             <span className="badge badge-danger">HOT</span>
                             <span className="badge badge-pill badge-secondary">$9.99</span>
                         </h2>
-                        <p className="d-none d-sm-block">A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.</p>
+                        <p className="d-none d-sm-block">{dish.description}</p>
 
                     </div>
                 </div>
@@ -42,10 +46,10 @@ function Home(props){
                         <h2 className='mt-0'>Great Food Variety
                             <span className="badge text-white">NEW</span>
                         </h2>
-                        <p className="d-none d-sm-block">Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person </p>
+    <p className="d-none d-sm-block">{promo.description}</p>
 
                     </div>
-                    <img className="img-fluid img-thumbnail align-self-center" src={food} alt="Buffet" />
+                    <img className="img-fluid img-thumbnail align-self-center" src={promo.image} alt="Buffet" />
                 </div>
             </div>
             </div>
@@ -57,11 +61,11 @@ function Home(props){
             </div>
             <div className="col col-sm order-sm-first col-md">
                 <div className="media">
-                    <img className="d-flex mr-3 img-thumbnail align-self-center" src={ahsanshah} alt="Ahsan Shah" width={150} height={150} />
+                    <img className="d-flex mr-3 img-thumbnail align-self-center" src={leader.image} alt="Ahsan Shah" width={150} height={150} />
                     <div className="media-body">
-                        <h2 className='mt-0'>Ahsan Shah</h2>
-                        <h4>Executive Chef</h4>
-                        <p className="d-none d-sm-block">Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. </p>
+                        <h2 className='mt-0'>{leader.name}</h2>
+                        <h4>{leader.designation}</h4>
+                        <p className="d-none d-sm-block">{leader.description} </p>
 
                     </div>
                 </div>
